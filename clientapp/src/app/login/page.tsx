5e9507e1dde0704cd69f53d4602fb836a956575e
Syed Abdullah Hassan <56790074+Abdullah12has@users.noreply.github.com>
2025-05-01 22:45:06 +0300

    some UI components added

---
+++ clientapp/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { LampDemo } from "@/components/lampDemo";
+import { SidebarDemo } from "@/components/sidebar";
+import Image from "next/image";
+
+export default function Login() {
+    return (
+        <div className="w-screen h-screen flex flex-col  bg-[#0E0E10]">
+            <SidebarDemo />
+
+        </div>
+    );
+}
